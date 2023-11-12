6b6227d6598d85a6e7daa01d3824b7fb5df43cc9
Adesina Joshua <tolujosh1@gmail.com>
2023-11-12 16:24:04 +0100

    Final commit

--- Desktop/Projects/Django user authentication/website/main/models.py
+++ Desktop/Projects/Django user authentication/website/main/models.py
@@ -8,6 +8,6 @@ class Post(models.Model):
     description = models.TextField()
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
-    
+
     def __str__(self):
         return self.title + "\n" + self.description
